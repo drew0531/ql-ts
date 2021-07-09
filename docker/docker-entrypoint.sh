e7c054cafca83a3ceb3028fcbf8367d2c008f15a
drew0531 <399896278jj@gmail.com>
2021-07-08 17:08:33 -0700

    first commit

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+dir_shell=/ql/shell
+. $dir_shell/share.sh
+link_shell
+echo -e "======================1. 检测配置文件========================\n"
+fix_config
+cp -fv $dir_root/docker/front.conf /etc/nginx/conf.d/front.conf
+echo
+
+echo -e "======================2. 更新源代码========================\n"
+ql update "no-restart"
+pm2 l >/dev/null 2>&1
+echo
+
+echo -e "======================3. 启动nginx========================\n"
+nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
+echo -e "nginx启动成功...\n"
+
+echo -e "======================4. 启动控制面板========================\n"
+if [[ $(pm2 info panel 2>/dev/null) ]]; then
+  pm2 reload panel
+else
+  pm2 start $dir_root/build/app.js -n panel
+fi
+echo -e "控制面板启动成功...\n"
+
+echo -e "======================5. 启动定时任务========================\n"
+if [[ $(pm2 info schedule 2>/dev/null) ]]; then
+  pm2 reload schedule
+else
+  pm2 start $dir_root/build/schedule.js -n schedule
+fi
+echo -e "定时任务启动成功...\n"
+
+if [[ $AutoStartBot == true ]]; then
+  echo -e "======================6. 启动bot========================\n"
+  ql bot
+fi
+
+echo -e "======================6. 启动JDC========================\n"
+if [[ $ENABLE_WEB_JDC == true ]]; then
+        pm2 start JDC
+        echo -e "JDC面板启动成功...\n"
+elif [[ $ENABLE_WEB_JDC == false ]]; then
+    echo -e "已设置为不自动启动JDC面板，跳过...\n"
+fi
+
+echo -e "############################################################\n"
+echo -e "容器启动成功..."
+echo -e "\n请先访问5700端口，登录成功面板之后再执行添加定时任务..."
+echo -e "\n请先访问5701端口，登录成功面板之后再执行添加扫码任务..."
+echo -e "############################################################\n"
+
+crond -f >/dev/null
+
+exec "$@"
---
+++ src/version.ts
@@ -0,0 +1,2 @@
+export const version = 'v2.2.0-end';
+export const changeLog = 'https://t.me/jiaolongwang/109';
